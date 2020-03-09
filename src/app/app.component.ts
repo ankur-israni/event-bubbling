307a52f192bd89e92f77bc1a0f77c6d380e8fbc6
Ankur Israni <ankur.israni@aa.com>
2020-03-09 01:13:53 -0500

    stable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'event-bubbling';
+
+  onButtonClick($event){
+    console.log("Buutton clicked", $event);
+  }
+  onDivClick($event){
+    console.log("Div clicked",$event);
+  }
 }
